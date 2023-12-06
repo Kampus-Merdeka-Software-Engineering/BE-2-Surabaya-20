e9376d8c0b97a619f3e7cdb4ba2e930f7a0c3135
FelixWahyu <felixwahyusejati89@gmail.com>
2023-12-06 15:17:45 +0700

    add app use express json
    menambahkan use express pada json

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const menusRoutes = require("./routes/menusRoutes");
 
